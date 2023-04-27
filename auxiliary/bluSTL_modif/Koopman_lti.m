a483c5d3bc4fb6a1cfee07c91eed21a1d77f8487
Abdu-Hekal <b6062805@ncl.ac.uk>
2023-04-27 03:39:14 +0100

    test changes

--- auxiliary/bluSTL_modif/Koopman_lti.m
+++ auxiliary/bluSTL_modif/Koopman_lti.m
@@ -4,9 +4,12 @@ classdef Koopman_lti
         dt %time_step
 
         xlabel %default label name bluSTL
+        nx %number of state variables
+        L %number of control points
         stlList %stl list to falsify
         cpBool %boolean array representing cp points
 
+        bigM %bigM value for milp
         solver_options %milp solver settings
 
         %milp sdpvars and constraints
@@ -20,17 +23,14 @@ classdef Koopman_lti
         %optimizer object
         milp
     end
-    properties (Dependent)
-        bigM %bigM value for milp
-        nx %number of state variables
-        L %number of control points
-    end
 
     methods
         % Constructor
         function Sys = Koopman_lti(reachZonos,dt)
             Sys.reachZonos = reachZonos;
             Sys.dt=dt;
+            Sys.nx=size(reachZonos{1}.center,1);
+            Sys.L=size(reachZonos,1)-1;
 
             % default label names
             Sys.xlabel = cell(1,Sys.nx);
@@ -44,27 +44,29 @@ classdef Koopman_lti
             gapLimit = 0.01;
             gapAbsLimit = 0.1;
             solnLimit = Inf;
-            verb = 0;
-%             Sys.solver_options = sdpsettings('verbose', verb,'solver', solver, ...
-%                 'gurobi.TimeLimit', timeLimit, ...
-%                 'gurobi.MIPGap', gapLimit, ...
-%                 'gurobi.MIPGapAbs', gapAbsLimit, ...
-%                 'gurobi.SolutionLimit', solnLimit,...
-%                 'gurobi.ScaleFlag', 2, ...
-%                 'usex0',1, ...
-%                 'cachesolvers',1,...
-%                 'gurobi.presolve',1);
-                 %gurobi.DualReductions',0
-             Sys.solver_options = sdpsettings('verbose', verb,'solver', solver, ...
-            'gurobi.TimeLimit', timeLimit, ...
-            'gurobi.MIPGap', gapLimit, ...
-            'gurobi.MIPGapAbs', gapAbsLimit, ...
-            'gurobi.SolutionLimit', solnLimit,...
-            'cachesolvers',1,...
-            'gurobi.BarHomogeneous', 1,...
-            'gurobi.ScaleFlag', 2, ...
-            'usex0',1, ...
-            'gurobi.DualReductions', 0);
+            verb = 1;
+            Sys.solver_options = sdpsettings('verbose', verb,'solver', solver, ...
+                'gurobi.TimeLimit', timeLimit, ...
+                'gurobi.MIPGap', gapLimit, ...
+                'gurobi.MIPGapAbs', gapAbsLimit, ...
+                'gurobi.SolutionLimit', solnLimit,...
+                'cachesolvers',1,...
+                'gurobi.BarHomogeneous', 1,...
+                'gurobi.ScaleFlag', 2, ...
+                'usex0',1, ...
+                'gurobi.DualReductions', 0, ...
+                'gurobi.TuneTrials',0,... %default
+                'gurobi.PreSOS2BigM',-1,... %default
+                'gurobi.CrossoverBasis',-1); %default
+%                 'gurobi.NumericFocus',1,...
+            Sys.solver_options = sdpsettings('verbose', verb,'solver', solver, ...
+                'gurobi.TimeLimit', timeLimit, ...
+                'gurobi.MIPGap', gapLimit, ...
+                'gurobi.MIPGapAbs', gapAbsLimit, ...
+                'gurobi.SolutionLimit', solnLimit,...
+                'cachesolvers',1,...
+                'usex0',1);
+
         end
 
         function Sys = setupAlpha(Sys)
@@ -79,30 +81,24 @@ classdef Koopman_lti
             Sys = KoopmanSetupReach(Sys);
         end
 
-        function value = get.nx(Sys)
-            value=size(Sys.reachZonos{1}.center,1);
-        end
-
-        function value = get.L(Sys)
-            value=size(Sys.reachZonos,1)-1;
-        end
-
-        function value = get.bigM(Sys)
+        %reach zonos setter, which also sets bigM value
+        function Sys=set.reachZonos(Sys,reachZonos)
+            Sys.reachZonos=reachZonos;
             %find suitable bigM based on zonotope boundaries
-            value=0;
-            for i=1:length(Sys.reachZonos)
-                zono=Sys.reachZonos{i};
-                if norm(zono,inf) > value
+            Sys.bigM=0;
+            for i=1:length(reachZonos)
+                zono=reachZonos{i};
+                if norm(zono,inf) > Sys.bigM
                     order = ceil(log10(norm(zono)));
-                    value = 10^order;
+                    Sys.bigM = 10^order;
                 end
             end
         end
 
         function diagnostics = optimize(Sys)
-
+            
             constraints=[Sys.Falpha, Sys.Fstl, Sys.Freach];
-            objective=Sys.Pstl;%objective is to miniimize robustness
+            objective=Sys.Pstl; %objective is to miniimize robustness
             options=Sys.solver_options;
             %% call solverarch
             diagnostics = optimize(constraints,objective,options);
