28289c52567cb4b0b47ee40487a2113391a3d327
abdu-hekal <b6062805@ncl.ac.uk>
2023-11-02 12:44:11 +0000

    added missing file

---
+++ examples/ARCHcomp22/printInfo.m
@@ -0,0 +1,70 @@
+function printInfo(solns,j)
+% soln: cells of kfModel.soln, see repArch22 for example
+
+avgFalsified=sum(getMetrics(solns,'falsified'));
+avgKoopTime=mean(getMetrics(solns,'koopTime'));
+avgMilpSetupTime=mean(getMetrics(solns,'milpSetupTime'));
+avgMilpSolveTime=mean(getMetrics(solns,'milpSolvTime'));
+avgSimTime=mean(getMetrics(solns,'simTime'));
+avgRuntime=mean(getMetrics(solns,'runtime'));
+sims=getMetrics(solns,'sims');
+avgSims=mean(sims);
+medianSims=median(sims);
+upper_quart = upper_quartile(sims);
+lower_quart = lower_quartile(sims);
+upper_whisk = upper_whisker(sims);
+lower_whisk = lower_whisker(sims);
+outliers = comp_outliers(sims, upper_whisk, lower_whisk);
+
+fprintf('Number of successful falsified traces: %d/%d\n', avgFalsified,j);
+disp('Computation Time -->');
+fprintf('Avg koopman time: %.2f seconds\n', avgKoopTime);
+fprintf('Avg milp setup time: %.2f seconds\n', avgMilpSetupTime);
+fprintf('Avg milp solve time: %.2f seconds\n', avgMilpSolveTime);
+fprintf('Avg simulation time: %.2f seconds\n', avgSimTime);
+fprintf('Avg total runtime: %.2f seconds\n', avgRuntime);
+fprintf('R: %.2f\n',100*avgSimTime/avgRuntime)
+disp('Simulations -->');
+fprintf('average: %.2f\n', avgSims);
+fprintf('median: %.2f\n', medianSims);
+fprintf('upper quartile= %.2f\n', upper_quart);
+fprintf('lower quartile= %.2f\n', lower_quart);
+fprintf('upper whisker= %.2f\n', upper_whisk);
+fprintf('lower whisker= %.2f\n', lower_whisk);
+fprintf('Outliers= [%.2f]\n', outliers);
+
+end
+
+function list = getMetrics(solns,metric)
+list=[];
+for i=1:length(solns{1})
+    list=[list,solns{1}{i}.(metric)];
+end
+end
+
+function res=upper_quartile(data)
+res= prctile(data, 75);
+end
+
+function res=lower_quartile(data)
+res= prctile(data, 25);
+end
+
+function res=upper_whisker(data)
+iqr = upper_quartile(data) - lower_quartile(data);
+res= min(upper_quartile(data) + 1.5 * iqr, max(data));
+end
+
+function res=lower_whisker(data)
+iqr = upper_quartile(data) - lower_quartile(data);
+res= max(lower_quartile(data) - 1.5 * iqr, min(data));
+end
+
+function res=comp_outliers(data, upper_whisk, lower_whisk)
+res = [];
+for ii=1:numel(data)
+    if data(ii) > upper_whisk || data(ii) < lower_whisk
+        res = [res data(ii)];
+    end
+end
+end
